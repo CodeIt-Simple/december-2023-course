6c3419698ce964be6a69789f7e03e4cb9c327d34
almoghr <almoghr36@gmail.com>
2024-01-04 17:44:28 +0200

    5th lesson has been taught

---
+++ lesson 5 - js basics/main.js
@@ -0,0 +1,116 @@
+// console.log("hello world!")
+// alert("hello Daniel")
+// prompt("enter your age")
+// console.log("hello Asaf")
+
+// const game = "GTA6"
+
+// console.log(game)
+
+// game = "FALLOUT-V"
+
+// console.log(typeof "something")
+// console.log(typeof 5)
+// console.log(typeof {})
+// console.log(typeof [])
+// console.log(typeof true)
+
+console.log(12 % 3);
+console.log(14 % 3);
+console.log(-(-3));
+
+console.log(+"1236349גדש");
+console.log(+"1236349");
+
+console.log(2 ** 3);
+console.log(100 ** -1);
+
+let counter = 0;
+console.log(counter);
+counter++;
+console.log(counter);
+counter--;
+console.log(counter);
+
+console.log("hello" + " " +"world")
+console.log("hello" + " world")
+console.log("hello "+"world")
+console.log("hello"+"world")
+
+console.log(3 + "450")
+console.log(3450)
+
+console.log(1+2+3+4+"5")
+console.log(10+20+"30"+40+50)
+console.log(10+20+"30"+(40+50))
+
+const a = '2'
+const b = 2
+
+console.log(a==b) // do not use!!
+
+console.log(a===b)
+
+console.log(a!==b)
+
+console.log(!(a===b))
+
+console.log(!!(a))
+console.log(!!(0))
+
+let x = 5
+
+x = x + 6
+x+=6
+x*=2
+x/=3
+x-=1
+
+
+const hobby = 'cards'
+
+if(hobby === 'chess'){
+    console.log('something')
+} else if (hobby === 'tv'){
+    console.log('go watch tv')
+} else if (hobby === "cards"){
+    console.log("go play poker")
+} else{
+    console.log("nothing from above")
+}
+
+let d;
+
+console.log(typeof d, typeof null)
+ 
+
+// בהתחשב בסטטוס הדתי שלך (חילוני, דתי, חרדי, אתאיסט)
+// ובהתחשב ביום בשבוע, נכתוב תכנית שאומרת לנו בקונסול מה אנחנו צריכים לעשות היום בצהריים.
+
+const today = 'sunday'
+const religiousStatus = 'atheist'
+
+if(today === 'sunday'){
+    if(religiousStatus === 'haredi'){
+
+    } else if(religiousStatus === 'dati'){
+
+    } else{
+        console.log('go to work')
+    }
+} else if( today === 'monday'){
+    
+}else if( today === 'tuesday'){
+    
+}else if( today === 'wednesday'){
+    
+}else if( today === 'thursday'){
+    
+}else if( today === 'friday'){
+    
+}else{
+    
+}
+
+const number = +prompt("enter a number") 
+console.log(number)
