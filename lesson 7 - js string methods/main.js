6cc99c556468878d9e45d0805eeb14454c2e29fd
almoghr <almoghr36@gmail.com>
2024-01-04 21:24:07 +0200

    string methods completed!

---
+++ lesson 7 - js string methods/main.js
@@ -0,0 +1,59 @@
+// const str = "hello";
+// console.log(str.length);
+
+// const phrase = "נחמן מאומן";
+// let phrase2 = "";
+// for (let i = 0; i < phrase.length; i++) {
+//   phrase2 += phrase[i];
+//   console.log(phrase2);
+// }
+
+// const myFirstArrival = (str, char) => {
+//   for (let i = 0; i < str.length; i++) {
+//     if (str[i] === char) {
+//       return i;
+//     }
+//   }
+//   return -1;
+// };
+
+// console.log(myFirstArrival("hello i am charlie", "z"));
+
+// const firstName = "  DAVID  ";
+// const lastName = "  AmihAi  ";
+
+// const trimmedFirstName = firstName.trim();
+// const trimmedLastName = lastName.trim();
+// console.log(!!' ')
+// console.log(trimmedFirstName)
+// const fullname = `${trimmedFirstName.slice(0, 1).toUpperCase()}${trimmedFirstName.slice(1)
+//   .toLowerCase()} ${trimmedLastName.slice(0, 1).toUpperCase()}${trimmedLastName.slice(1).toLowerCase()}`;
+
+//   console.log(fullname)
+
+
+// const lorem = `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum`
+// let newLorem = '';
+
+// for(let i = 0; i < lorem.length; i++){
+//     if((lorem[i] !== ' ') && (lorem[i] !== '.') && (lorem[i] !== ',') && (lorem[i] !== `'`)){
+//         newLorem += lorem[i] + lorem[i]
+//     } else{
+//         newLorem += lorem[i]
+//     }
+// }
+
+// console.log(newLorem)
+
+// const str = 'abcdefgfedcba'
+// const dad = 'aba'
+
+// console.log(str.indexOf('a'))
+// console.log(str.lastIndexOf('a'))
+
+// console.log(str.indexOf('b'))
+// console.log(str.lastIndexOf('b'))
+
+// console.log(str.indexOf('g'))
+// console.log(str.lastIndexOf('g'))
+
