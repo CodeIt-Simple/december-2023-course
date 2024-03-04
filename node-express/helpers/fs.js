4eafeabd61d643f00215a43c288dca669a132009
almoghr <almoghr36@gmail.com>
2024-03-04 19:40:45 +0200

    mongo + mongoose + resetDB works

--- node-express/helpers/fs.js
+++
@@ -1,7 +0,0 @@
-import fs from 'fs'
-
-export const getAllUsers = () => fs.readFileSync("./users.json", "utf8");
-
-export const changeJson = (array) => {
-    fs.writeFileSync("./users.json", JSON.stringify(array));
-}
--- node-express/server.js
+++ node-express/server.js
@@ -1,9 +1,19 @@
 import express from "express";
 import cors from "cors";
-import { changeJson, getAllUsers } from "./helpers/fs.js";
-const port = 3000;
+import mongoose from "mongoose";
 
+const port = 3000;
+const dbPort = 27017;
 const app = express();
+
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  age: { type: Number, required: true },
+  hobby: { type: String, default: "no hobby chosen yet" },
+  dateCreated: { type: Date, default: Date.now() },
+});
+
+export const User = mongoose.model("User", userSchema);
 
 app.use(express.json());
 // dangerous
@@ -11,57 +21,98 @@ app.use(cors());
 
 // GET
 // get all users from json
-app.get('/', (req, res) => {
-    const allUsers = JSON.parse(getAllUsers())
-    res.status(200).send(allUsers)
-})
+app.get("/", async (req, res) => {
+  try {
+    const allUsers = await User.find({});
+    if (allUsers.length === 0 || !allUsers) {
+      return res.status(204).send({ message: "No users found" });
+    }
+    return res.status(200).send(allUsers);
+  } catch (e) {
+    return res.status(500).send({ message: e.message });
+  }
+});
 
 // GET
 // get a single user by id from the params
-app.get('/users/:id', (req, res) => {
-    const allUsers = JSON.parse(getAllUsers())
-    const id = req.params.id
-    const user = allUsers.find(usr => usr.id === +id)
-    res.status(200).send(user)
-
-})
+app.get("/users/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const user = await User.findOne({ _id: id });
+    if (!user) {
+      return res.status(404).send({ message: "no user found" });
+    }
+    return res.status(200).send(user);
+  } catch (e) {
+    return res.status(500).send({ message: e.message });
+  }
+});
 
 // POST
-// create user 
-app.post('/createUser/', (req, res) => {
-    const allUsers = JSON.parse(getAllUsers())
-    const userForm = {...req.body}
-    allUsers.push(userForm)
-    changeJson(allUsers)
-    res.status(200).send(allUsers)
-
-
-})
+// create user
+app.post("/createUser/", async (req, res) => {
+  try {
+    const userForm = { ...req.body };
+    const user = new User(userForm);
+    await user.save();
+    res.status(200).send(user);
+  } catch (e) {
+    return res.status(500).send({ message: e.message });
+  }
+});
 
 //PUT
 // updates a user
-app.put('/updateUser', (req, res) => {
-    const allUsers = JSON.parse(getAllUsers());
-    const userUpdatesForm = {...req.body}
-    const userToBeUpdatedIndex = allUsers.findIndex(user => user.id === userUpdatesForm.id)
-    allUsers.splice(userToBeUpdatedIndex, 1, userUpdatesForm)
-    changeJson(allUsers)
-    res.status(200).send(allUsers)
+app.put("/updateUser/:id", async (req, res) => {
+  const userAllowedUpdates = ["age", "hobby"];
 
-})
+  const updates = Object.keys(req.body);
+
+  const isValidOperation = updates.every((update) =>
+    userAllowedUpdates.includes(update)
+  );
+
+  if (!isValidOperation) {
+    return res.status(400).send({ message: "Invalid updates" });
+  }
+
+  try {
+    const id = req.params.id;
+    const user = await User.findOne({ _id: id });
+
+    if (!user) {
+      return res.status(404).send({ message: "user does not exist" });
+    }
+
+    updates.forEach((update) => (user[update] = req.body[update]));
+    await user.save();
+    return res.status(200).send(user);
+  } catch (e) {
+    return res.status(500).send({ message: e.message });
+  }
+});
 
 //DELETE
 // DELETES a user
 
-app.delete('/deleteUser/:id', (req, res) => {
-    const allUsers = JSON.parse(getAllUsers());
-    const id = req.params.id
+app.delete("/deleteUser/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const deletedUser = await User.findOneAndDelete({ _id: id });
+    if (!deletedUser) {
+      return res.status(404).send({ message: "no user found" });
+    }
+    return res.status(200).send(deletedUser);
+  } catch (e) {
+    return res.status(500).send({ message: e.message });
+  }
+});
 
-    const allUsersAfterDeletion = allUsers.filter(user => user.id !== +id)
-    changeJson(allUsersAfterDeletion)
-    res.status(200).send(allUsersAfterDeletion)
-})
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+app.listen(port, async () => {
+  try {
+    await mongoose.connect(`mongodb://localhost:${dbPort}/CodeItSimpleShop`);
+    console.log(`Example app listening on port ${port}`);
+  } catch (e) {
+    console.log(e);
+  }
 });
